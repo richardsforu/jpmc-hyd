c03970205bf2f382fabedb16b16bad38eb650df0
ctsjava <ctsgit@gmail.com>
2019-04-10 13:16:37 +0530

    Spring boot added

---
+++ Spring Boot/Spring-BOOT-WEB-EMPLOYEE/src/main/java/com/cts/employee/SpringBootWebEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.cts.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebEmployeeApplication.class, args);
+	}
+
+}
